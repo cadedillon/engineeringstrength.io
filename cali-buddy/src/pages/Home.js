5b47ae285151d9952c655a4c67137cf1f4509976
Cade Dillon <cade.dillon@outlook.com>
2024-06-19 18:42:18 -0700

    Removed default React app starter files, added rough home page and favicon

---
+++ cali-buddy/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Calibuddy</h1>
+      <p>
+        Our goal is to help you optimize your calisthenics and gymnastics
+        training.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
